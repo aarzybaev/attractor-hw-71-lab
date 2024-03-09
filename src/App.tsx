a57b93a7c3800e9afd0225bbc7bc84f101bd6c2c
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-09 10:37:32 +0600

    removed unnecessary component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Layout from './components/Layout/Layout';
 import {Route, Routes} from 'react-router-dom';
 import Dishes from './components/Dishes/Dishes';
 import Orders from './containers/Orders';
-import Admin from './containers/Admin';
 import DishNew from './containers/DishNew';
 import DishEdit from './containers/DishEdit';
 import Home from './containers/Home';
@@ -14,7 +13,7 @@ const App = () => {
       <Layout>
         <Routes>
           <Route path="/" element={(<Home />)} />
-          <Route path="/admin" element={(<Admin />)} />
+          <Route path="/admin" element={(<Dishes />)} />
           <Route path="/admin/dishes" element={(<Dishes />)} />
           <Route path="/admin/dishes/new-dish" element={(<DishNew />)} />
           <Route path="/admin/dishes/edit/:id" element={(<DishEdit />)} />
