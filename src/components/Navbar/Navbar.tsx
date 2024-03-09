72ef0f49fb9a47438e6193e70a93a38f62c140be
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-09 10:23:19 +0600

    improved logic

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import {NavLink, useLocation} from 'react-router-dom';
 const Navbar: React.FC = () => {
+  const location = useLocation();
+  const isHome = (location.pathname === '/');
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container-fluid">
-        <NavLink to="/admin" className="navbar-brand">Turtle pizza :: Admin</NavLink>
-        <div className="collapse navbar-collapse">
+        <NavLink to="/" className="navbar-brand">Turtle pizza {!isHome ? ":: Admin": ""}</NavLink>
+        {!isHome && <div className="collapse navbar-collapse">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <NavLink className="nav-link" to="/admin/dishes">Dishes</NavLink>
@@ -14,7 +16,7 @@ const Navbar: React.FC = () => {
               <NavLink className="nav-link" to="/admin/orders">Orders</NavLink>
             </li>
           </ul>
-        </div>
+        </div>}
       </div>
     </nav>
   );
